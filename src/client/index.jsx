509307e5785435d3f8e78a3b9a72a41797c18f50
Chiel Kunkels <chiel@kunkels.me>
2016-09-29 17:11:20 +0200

    condense the example

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,5 +1,12 @@
-import App from 'app/components/App';
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+function App() {
+	return (
+		<div className="app">
+			app
+		</div>
+	);
+}
 
 ReactDOM.render(<App />, document.getElementById('app-root'));
